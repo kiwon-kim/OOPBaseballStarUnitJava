fd2b938ac1a0dca80d2ed04147b837b2990e6104
cplkimth <cplkimth@naver.com>
2019-10-31 15:37:34 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
-
     public static void main(String[] args) {
-        // 알고리즘 기술
-        System.out.println("Hello World from Home");
+        // 1. 정답을 생성한다.
+
+
+        // 2. 추측을 입력받는다.
+
+
+        // 3. 결과를 계산한다.
+
+
+        // 4. 결과를 출력한다.
+
+
+        // 5. 3S 아니면 2번으로 돌아간다.
+
+
+        // 6. 도전횟수를 출력한다.
     }
 }
